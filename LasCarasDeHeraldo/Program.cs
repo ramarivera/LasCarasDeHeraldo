d826cf4f4ec6f91d729fbf9623206ca619513ca5
Agustina Mannise <agusmn95@gmail.com>
2015-11-10 23:01:33 -0300

    holaaabbbbbbb

--- LasCarasDeHeraldo/Program.cs
+++ LasCarasDeHeraldo/Program.cs
@@ -14,7 +14,7 @@ namespace LasCarasDeHeraldo
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
+            Application.EnableVisualStyles(); 
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Login());
         }
